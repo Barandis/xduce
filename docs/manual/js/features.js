cf6e4545c6588bccc1af1dddd9ec2b9af0483068
Thomas Otterson <barandis1729@gmail.com>
2017-09-02 15:28:00 -0400

    docs(manual): Added features page
    Added a quick list of features to be at the beginning of the manual. Issue #15.

---
+++ docs/manual/js/features.js
@@ -0,0 +1,10 @@
+/* global load */
+
+try {
+  load('../include/header.html', 'body > header');
+  load('include/nav.html', '#content > nav', 2);
+  load('../include/footer.html', 'body > footer');
+  load('include/aside.html', '#content > aside', 2);
+} catch (e) {
+  document.querySelector('.ie-message').style.display = 'table-cell';
+}
