a3085cae985ab771a7e392837f64726e9d2263a8
Thomas Otterson <barandis1729@gmail.com>
2017-09-04 19:00:49 -0400

    docs(manual): Changed theory page on feedback
    Layla suggested some things that weren't easy to understand, so I tried to improve those things.
    Also changed structure of layout (nav specifically) for future preparation. Issue #15.

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,8 @@
 /* global load */
+
 try {
   load('include/header.html', 'body > header');
-  load('include/nav.html', '#content > nav', 1);
+  load('include/main.html', '#content > nav.main', 1);
   load('include/footer.html', 'body > footer');
 } catch (e) {
   document.querySelector('.ie-message').style.display = 'table-cell';
--- docs/manual/js/features.js
+++ docs/manual/js/features.js
@@ -2,9 +2,9 @@
 
 try {
   load('../include/header.html', 'body > header');
-  load('include/nav.html', '#content > nav', 2);
+  load('include/main.html', '#content > nav.main', 2);
   load('../include/footer.html', 'body > footer');
-  load('include/aside.html', '#content > aside', 2);
+  load('include/aux.html', '#content > nav.aux', 2);
 } catch (e) {
   document.querySelector('.ie-message').style.display = 'table-cell';
 }
